c1ab138a9a7e6415b094665730b4c527c3e1e1f1
jafet <123048167@upq.edu.mx>
2026-03-03 20:11:24 -0600

    Practica No.6: Protección de Endpoints

--- miAPI/app/main.py
+++ miAPI/app/main.py
@@ -1,101 +1,121 @@
-#importaciones
-from fastapi import FastAPI, status, HTTPException
+# Importaciones
+from fastapi import FastAPI, status, HTTPException, Depends
 import asyncio
 from typing import Optional
+from pydantic import BaseModel, Field
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import secrets
 
-#instancia del servidor
+# Instancia del servidor 
 app = FastAPI(
-    title="MI PRIMER API",
-    description="Esta es la API de jafet uribe",
+    title="Mi primer API",
+    description="Jafet uribe uribe",
     version="1.0.0"
-    )
+)
 
-#BD ficticia
-usuarios=[
-    {"id":1,"nombre":"rogelio","edad":21},
-    {"id":2,"nombre":"gabo jobs","edad":35},
-    {"id":3,"nombre":"isaac","edad":21},
+# BD ficticia
+usuarios = [
+    {"id": 1, "nombre": "Juan", "edad": 21},
+    {"id": 2, "nombre": "Isra", "edad": 23},
+    {"id": 3, "nombre": "Abdiel", "edad": 21},
+    {"id": 4, "nombre": "Jafet", "edad": 24},
+    {"id": 5, "nombre": "Roger", "edad": 19},
 ]
 
-#Endpoints
-@app.get("/",tags=['Inicio'])
-async def root():
-    return {"mensaje": "Bienvenido a mi API"}
+# Modelo de validación pydantic
+class usuario_create(BaseModel):
+    id: int = Field(..., gt=0, description="Identificador de usuario")
+    nombre: str = Field(..., min_length=3, max_length=50, json_schema_extra={"example": "Jafet"})
+    edad: int = Field(..., ge=1, le=123, description="Edad valida entre 1 - 123")
 
-@app.get("/HolaMundo",tags=['Bienvenida Asincrona'])
+# Seguridad
+security = HTTPBasic()
+
+def verificar_Peticion(credenciales: HTTPBasicCredentials = Depends(security)):
+    userAuth = secrets.compare_digest(credenciales.username, "jafeturibe")
+    passAuth = secrets.compare_digest(credenciales.password, "123456")
+
+    if not (userAuth and passAuth):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Credenciales no Autorizadas"
+        )
+    return credenciales.username
+
+# Endpoints
+@app.get("/", tags=["Inicio"])
+async def bienvenida():
+    return {"mensaje": "¡Bienvenido a mi API!"}
+
+@app.get("/HolaMundo", tags=["Bienvenida Asincrona"])
 async def hola():
-    await asyncio.sleep(3)
+    await asyncio.sleep(4)
     return {
-        "mensaje": "Hola Mundo FastAPI",
-        "estatus":  "200"
-        }
-
-@app.get("/v1/parametroOb/{id}",tags=['Parametro oblogatorio'])
-async def consultaUno(id:int):
-    return {"Se encontro usuario": id,
-         }
-
-@app.get("/v1/parametroOp/",tags=['Parametro Opcional'])
-async def consultaTodos(id:Optional[int]=None):
-    if id is not None:
-        for usuario in usuarios:
-            if usuario["id"]== id:
-                return {"mensaje":"usuario encontrado","usuario":usuario}
-        return {"mensaje":"usuario no encontrado","usuario":id}
-    else:
-        return {"mensaje":"No se proporciono id"}
-
-
-
-@app.get("/v1/usuarios/",tags=['CRUD HTTP'])
-async def leer_uruarios():
-    return{
-        "status":"200",
-        "total": len(usuarios),
-        "usuarios":usuarios
+        "mensaje": "¡Hola mundo FastAPI! ",
+        "estatus": "200"
     }
 
-@app.post("/v1/usuarios/",tags=['CRUD HTTP'],status_code=status.HTTP_201_CREATED)
-async def crear_uruario(usuario:dict):
+@app.get("/v1/parametroOp/{id}", tags=["Parametro obligatorio"])
+async def consultaUno(id: int):
+    return {"Se encontro usuario": id}
+
+@app.get("/v1/parametroOp/", tags=["Parametro opcional"])
+async def consultaTodos(id: Optional[int] = None):
+    if id is not None:
+        for usuario in usuarios:
+            if usuario["id"] == id:
+                return {"mensaje": "usuario encontrado", "usuario": usuario}
+        return {"mensaje": "usuario no encontrado", "usuario": id}
+    else:
+        return {"mensaje": "No se proporciono id"}
+
+@app.get("/v1/usuarios/", tags=["CRUD HTTP"])
+async def leer_usuarios():
+    return {
+        "status": "200",
+        "total": len(usuarios),
+        "usuarios": usuarios
+    }
+
+@app.post("/v1/usuarios/", tags=["CRUD HTTP"], status_code=status.HTTP_201_CREATED)
+async def crear_usuario(usuario: usuario_create):
     for usr in usuarios:
-        if usr["id"] == usuario.get("id"):
+        if usr["id"] == usuario.id:
             raise HTTPException(
                 status_code=400,
                 detail="El id ya existe"
             )
-    usuarios.append(usuario)
-    return{
-        "mensaje":"Usuario Agregado",
-        "Usuario":usuario
+    
+    usuarios.append(usuario.model_dump())
+    return {
+        "mensaje": "Usuario agregado",
+        "Usuario": usuario
     }
 
-
-@app.put("/v1/usuarios/{id}", tags=['CRUD HTTP'], status_code=status.HTTP_204_NO_CONTENT)
-async def actualizar_usuario(id: int, usuario: dict):
-
+@app.put("/v1/usuarios/{id_buscado}", tags=["CRUD HTTP"])
+async def actualizar_usuario(id_buscado: int, datos_nuevos: dict):
     for usr in usuarios:
-        if usr["id"] == id:
-            usr.update(usuario)  
-            return  
-
+        if usr["id"] == id_buscado:
+            usr.update(datos_nuevos)
+            return {
+                "mensaje": "Usuario actualizado",
+                "usuario": usr
+            }
     raise HTTPException(
         status_code=404,
         detail="Usuario no encontrado"
     )
 
-@app.delete("/v1/usuarios/{id}", tags=['CRUD HTTP'], status_code=status.HTTP_204_NO_CONTENT)
-async def eliminar_usuario(id: int):
-
-    for usr in usuarios:
-        if usr["id"] == id:
-            usuarios.remove(usr)  
-            return  
-
+@app.delete("/v1/usuarios/{id}", tags=['CRUD HTTP'], status_code=status.HTTP_200_OK)
+async def eliminar_usuario(id: int, userAuth: str = Depends(verificar_Peticion)):
+    for usuario in usuarios:
+        if usuario["id"] == id:
+            usuarios.remove(usuario) 
+            return {
+                "message": f"Usuario eliminado por: {userAuth}",
+                "usuario_eliminado": usuario
+            }
     raise HTTPException(
-        status_code=404,
+        status_code=404, 
         detail="Usuario no encontrado"
     )
-
-
-
-
