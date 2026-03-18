478965fe6698ab240ede85aebb011e8ee4ae7a19
jafet <123048167@upq.edu.mx>
2026-03-17 20:17:32 -0600

    Practica No.8 : Router

---
+++ miAPI/app/models/usuario.py
@@ -0,0 +1,7 @@
+# Modelo de validación pydantic
+from pydantic import BaseModel, Field
+
+class usuario_create(BaseModel):
+    id: int = Field(..., gt=0, description="Identificador de usuario")
+    nombre: str = Field(..., min_length=3, max_length=50, json_schema_extra={"example": "Jafet"})
+    edad: int = Field(..., ge=1, le=123, description="Edad valida entre 1 - 123")
---
+++ miAPI/app/routers/usuarios.py
@@ -0,0 +1,63 @@
+
+#endpoints usuarios
+
+from fastapi import status, HTTPException, Depends, APIRouter
+from app.models.usuario import usuario_create
+from app.data.database import usuarios
+from app.security.auth import verificar_Peticion
+
+router = APIRouter(
+    prefix= "/v1/usuarios", tags=["CRUD HTTP"]
+)
+
+
+@router.get("/")
+async def leer_usuarios():
+    return {
+        "status": "200",
+        "total": len(usuarios),
+        "usuarios": usuarios
+    }
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def crear_usuario(usuario: usuario_create):
+    for usr in usuarios:
+        if usr["id"] == usuario.id:
+            raise HTTPException(
+                status_code=400,
+                detail="El id ya existe"
+            )
+    
+    usuarios.append(usuario.model_dump())
+    return {
+        "mensaje": "Usuario agregado",
+        "Usuario": usuario
+    }
+
+@router.put("/{id}")
+async def actualizar_usuario(id: int, datos_nuevos: dict):
+    for usr in usuarios:
+        if usr["id"] == id:
+            usr.update(datos_nuevos)
+            return {
+                "mensaje": "Usuario actualizado",
+                "usuario": usr
+            }
+    raise HTTPException(
+        status_code=404,
+        detail="Usuario no encontrado"
+    )
+
+@router.delete("/{id}", status_code=status.HTTP_200_OK)
+async def eliminar_usuario(id: int, userAuth: str = Depends(verificar_Peticion)):
+    for usuario in usuarios:
+        if usuario["id"] == id:
+            usuarios.remove(usuario) 
+            return {
+                "message": f"Usuario eliminado por: {userAuth}",
+                "usuario_eliminado": usuario
+            }
+    raise HTTPException(
+        status_code=404, 
+        detail="Usuario no encontrado"
+    )
