27df4301c25d757826102cbd7e76e79eb1f0565d
jafet <123048167@upq.edu.mx>
2026-03-18 18:56:01 -0600

    practica 9 primer commit

---
+++ miAPI/app/data/db.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+#definimos la URL de la BD
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://admin:123456@postgres:5434/DB_miapi"
+)
+
+#2. creamos el motor de conexion 
+engine= create_engine(DATABASE_URL)
+
+# creamos gestionador de sesiones
+SessionLocal= sessionmaker(
+    autocommit= False,
+    autoflush= False,
+    bind= engine
+)
